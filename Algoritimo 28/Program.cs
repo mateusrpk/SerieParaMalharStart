e342e830a88d6adcc99c337961ab28740219af1f
mateus <mateus@DESKTOP-2MVCGCQ>
2022-09-29 09:20:39 -0300

    Adicionar arquivos de projeto.

---
+++ Algoritimo 28/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieParaMalharStart.Algoritimo28
+{
+  internal class Progams
+    {
+    
+    static void Main(string[] arg)
+    
+    {
+
+    
+        
+        
+        
+        Console.WriteLine("Serviço Nacional de Apredizagem");
+            Console.ReadKey();
+        
+        }
+    }
+}
+
+
---
+++ SerieParaMalharStart.Alongamento/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieParaMalharStart.Alongamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //Alongamento
+          //Variável - Atribuição - Entrada e Saída
+          //Funções para String
+            Console.WriteLine("Digite o nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite a Média");
+            double media = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a idade");
+            int idade = int.Parse(Console.ReadLine());
+            string mensagem = idade < 18 ? "menor" : "menor";
+            Console.WriteLine($"{nome} sua media foi {media}");
+            Console.WriteLine($"{nome} você é {mensagem.ToUpper()}");
+            Console.ReadKey();
+        
+        }
+    }
+}
